8e9f623a537d1a706b6304cb6d21aae0c63024a8
larissa ayumi <166153696+BielXL2006@users.noreply.github.com>
2024-06-19 14:59:56 -0300

    adicionadas as funções de buscar as perguntas e montar as perguntas no html

--- helpers/tema-helper.js
+++ helpers/tema-helper.js
@@ -1,14 +1,14 @@
- let tema = "claro"
- 
- export function trocarTema(body, botaoTema) {
+let tema = "claro"
+
+export function trocarTema(body, botaoTema){
     if (localStorage.getItem("tema")) {
         tema = localStorage.getItem("tema")
     }
-
-    if (tema === "claro") {
-     body.classList.add("escuro")
-     localStorage.setItem("tema", "escuro")
-     botaoTema.style.justifyContent = "flex-end"
+    
+    if (tema === "claro"){
+        body.classList.add("escuro")
+        localStorage.setItem("tema", "escuro")
+        botaoTema.style.justifyContent = "flex-end"
     } else {
         body.classList.remove("escuro")
         localStorage.setItem("tema", "claro")
@@ -16,12 +16,12 @@
     }
 }
 
-export function verificarTema(body, botaoTema) {
-    if (localStorage.getItem("tema")) {
+export function verificarTema(body, botaoTema){
+    if (localStorage.getItem("tema")){
         tema = localStorage.getItem("tema")
     }
 
-    if (tema === "escuro") {
+    if (tema === "escuro"){
         body.classList.add("escuro")
         botaoTema.style.justifyContent = "flex-end"
     }
